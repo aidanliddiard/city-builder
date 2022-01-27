2b4103a7f3a56ce4d287a2020a121722e178d472
aidanliddiard <aidanliddiard@gmail.com>
2022-01-27 15:25:49 -0800

    function displayStats and createCountString working!

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // import functions and grab DOM elements
 
+import { createCountString } from '/test/functions.js';
+
 const climateDropdown = document.getElementById('climate');
 const climateImg = document.getElementById('climate-image');
+const climateStatsEl = document.getElementById('climate-stats');
 
 const cityDropdown = document.getElementById('city-size');
 const cityImg = document.getElementById('city-size-image');
+const cityStatsEl = document.getElementById('city-size-stats');
 
 const locationDropdown = document.getElementById('location');
 const locationImg = document.getElementById('location-image');
+const locationStatsEl = document.getElementById('location-stats');
 
 
 const sloganOutputEl = document.getElementById('slogan-output');
@@ -37,7 +42,7 @@ climateDropdown.addEventListener('change', (e) => {
 // city-size dropdown
 cityDropdown.addEventListener('change', (e) => {
     const value = e.target.value;
-    const type = 'city-size';
+    const type = 'city size';
     cityCount++;
     cityImg.src = `/assets/city-size-${value}.jpg`;
 
@@ -56,5 +61,17 @@ locationDropdown.addEventListener('change', (e) => {
 
 
 function displayStats(type, count) {
+    if (type === 'climate') {
+        const statsString = createCountString(type, count);
+        climateStatsEl.textContent = statsString;
+    }
+    else if (type === 'city size') {
+        const statsString = createCountString(type, count);
+        cityStatsEl.textContent = statsString;
+    }
+    else {
+        const statsString = createCountString(type, count);
+        locationStatsEl.textContent = statsString;
+    }
 
 }
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,32 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { createCountString } from "./functions.js";
+
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 'You clicked on climate 4 times.';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = createCountString('climate', 4);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('time to test a function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'You clicked on location 2 times.';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = createCountString('location', 2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
