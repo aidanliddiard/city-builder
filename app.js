bc75c0409335bdf332f4df8497a55ae701bcc49b
aidanliddiard <aidanliddiard@gmail.com>
2022-01-27 14:17:14 -0800

    grabing DOM elements

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // import functions and grab DOM elements
 
+const climateDropdown = document.getElementById('climate');
+const cityDropdown = document.getElementById('city-size');
+const locationDropdown = document.getElementById('location');
+
+
+const sloganOutputEl = document.getElementById('slogan-output');
+const sloganInputEl = document.getElementById('slogan-input');
+const sloganBtnEl = document.getElementById('slogan-btn');
+
+
+
 // let state
+
 
 // set event listeners 
   // get user input
